2ad8e2365a9e937a0bdd32e40fc2f255b96462ae
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-12-15 17:55:46 +0530

    refactor: remove movie routes and update API response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from "express";
 import animeRouter from "./anime";
-import moviesRouter from "./movies/tmdb";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.json([{ routes: ["/anime/", "/movies/"] }]);
+  res.json([{ routes: ["/anime/"] }]);
 });
 router.use("/anime", animeRouter);
-router.use("/movies/tmdb", moviesRouter);
 
 export default router;
